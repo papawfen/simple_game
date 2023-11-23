54980badde503d7e71b40df6b6c82bbcf3eaf95a
Papaw Fenrir <papawfen@si-e4.nsk.21-school.ru>
2023-11-24 02:18:32 +0700

    almost_done

---
+++ Classes/HelloWorldScene.cpp
@@ -0,0 +1,19 @@
+//#include "HelloWorldScene.h"
+//
+//USING_NS_CC;
+
+//Scene* HelloWorld::createScene() {
+//    return HelloWorld::create();
+//}
+//
+//bool HelloWorld::init() {
+//    if (!Scene::init()) {
+//        return false;
+//    }
+//    auto visibleSize = Director::getInstance()->getVisibleSize();
+//    Vec2 origin = Director::getInstance()->getVisibleOrigin();
+////    auto label = Label::createWithSystemFont("Hello World", "Arial", 48);
+////    label->setAnchorPoint(cocos2d::Vec2(0.0, 0.0));
+////    this->addChild(label, 1);
+//    return true;
+//}
